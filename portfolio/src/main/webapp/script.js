ba64d206fb5e3a060bad468eb48ef511d44a6c20
Catherine Yu <39574893+cyu6@users.noreply.github.com>
2020-06-30 17:11:30 -0400

    Merge cyuDevelopment branch into master branch (#10)
    * Blog page including Potion API and Tilt.js libraries
    
    * New line error in CSS file
    
    * Added projects page with styling (#3)
    
    * Added projects page with styling
    
    * Added another project to the page
    
    * CSS formatting fix 2 spaces instead of 4
    
    * Added projects page with styling
    
    * Added another project to the page
    
    * CSS formatting fix 2 spaces instead of 4
    
    * HTML/CSS formatting fixes to align with style guide
    
    * Added photo gallery with styling (#4)
    
    * Added projects page with styling
    
    * Added another project to the page
    
    * CSS formatting fix 2 spaces instead of 4
    
    * Added photo gallery with styling
    
    * Added projects page with styling
    
    * Added another project to the page
    
    * CSS formatting fix 2 spaces instead of 4
    
    * HTML/CSS formatting fixes to align with style guide
    
    * Added photo gallery with styling
    
    * HTML/CSS format fixes aligning with style guide
    
    * Typo in class name
    
    * Formatting fixes to photos.html and script.js
    
    * Updated JavaScript and CSS styling
    
    * Added fonts to CSS file
    
    * New line error fixes
    
    * Restyle projects page, add random page content, add blog post page (#6)
    
    * Added projects page with styling
    
    * Added another project to the page
    
    * CSS formatting fix 2 spaces instead of 4
    
    * Added projects page with styling
    
    * Added another project to the page
    
    * CSS formatting fix 2 spaces instead of 4
    
    * HTML/CSS formatting fixes to align with style guide
    
    * Revamp projects design and add random page content
    
    * Revamp projects page design and add random page content
    
    * Working around git commits
    
    * Add more cat pictures
    
    * Small styling fixes
    
    * Delete TODO that was completed

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -14,7 +14,8 @@
 
 const GREETING_CHOICES =
     ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!', 'How are you doing?', 'How do you do?'];
-const CAT_IMAGES = ["images/kitten-in-bed.jpg", "images/sleepy-kitten.jpg"];
+const CAT_IMAGES = ["images/kitten-in-bed.jpg", "images/sleepy-kitten.jpg", "images/kitten-covers.jpg", 
+                    "images/silver-tabby.jpg", "images/teddy-cat.jpg"];
 const POST_ID = "0cb628857f3c4c77bf7f9a879a6ec21d";
 
 /**
