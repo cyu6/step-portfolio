f9c5660ba194783e0aecdd9fa9b3f7a0cf3f75cf
Catherine Yu <catyu6000@gmail.com>
2020-07-06 20:57:05 +0000

    Removed unnecessary line

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -42,7 +42,6 @@ public class LoginServlet extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
 
     if (userService.isUserLoggedIn()) {
-      String userEmail = userService.getCurrentUser().getEmail();
       String logoutUrl = userService.createLogoutURL(REDIRECT_URL);
 
       response.getWriter().println("logged in");
